3a3a3b98f63e0e2975c8f72331d5fcd0bb1e7f4e
zayle <email@zayle.me>
2015-09-02 01:17:23 -0500

    training

---
+++ Project Euler/Solved/P067 - Maximum path sum II.cpp
@@ -0,0 +1,41 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+fstream fs("P067 - Maximum path sum II.in.txt", fstream::in);
+
+const int mx = 100;
+int triangle[mx + 1][mx];
+
+void LoadTriangle() {
+	int i, j;
+	for (i = 1; i <= mx; i++) {
+		for (j = 0; j < i; j++) {
+			fs >> triangle[i][j];
+		}
+	}
+}
+
+void Solve() {
+	int i, j, aux = 0;
+	for (i = 2; i <= mx; i++) {
+		triangle[i][0] += triangle[i - 1][0];
+		for (j = 1; j < (i - 1); j++) {
+			triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+		}
+		triangle[i][i - 1] += triangle[i - 1][i - 2];
+	}
+	for (i = 0; i < mx; i++) {
+		aux = max(aux, triangle[mx][i]);
+	}
+	cout << aux << '\n';
+}
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	int i, j;
+	// ========== //
+	LoadTriangle();
+	Solve();
+	return 0;
+}
