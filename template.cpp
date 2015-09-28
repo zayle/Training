dc2951d9971584e46344615d88ca0112d16a6d3a
zayle <email@zayle.me>
2015-09-28 00:28:07 -0500

    training

--- template.cpp
+++ template.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 fstream fs("in.txt", fstream::in);
 
+void Solve() {
+
+}
+
 int main() {
 	cin.tie(0);
 	ios_base::sync_with_stdio(0);
 	// ========== //
+	Solve();
 	return 0;
 }
